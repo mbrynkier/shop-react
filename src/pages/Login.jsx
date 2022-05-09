53f5f7269e6abfde8a6f5e96037c85098bcf206f
mbrynkier <mbrynkier@gmail.com>
2022-05-09 20:57:16 -0300

    finish course!

---
+++ src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React, {useRef} from 'react';
+import '../styles/Login.scss';
+
+const Login = () => {
+	const form = useRef(null)
+
+	const handleSubmit = (event) => {
+		event.preventDefault(); //para que no se refresque y mande los valores por html
+		const formData = new FormData(form.current);
+		const data = {
+			username: formData.get('email'),
+			password: formData.get('password')
+		}
+		console.log(data);
+	}
+
+	return (
+		<div className="Login">
+			<div className="Login-container">
+				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+				<form action="/" className="form" ref={form}>
+					<label htmlFor="email" className="label">Email address</label>
+					<input type="text" name="email" placeholder="yourMail@example.com" className="input input-email" />
+					<label htmlFor="password" className="label">Password</label>
+					<input type="password" name="password" placeholder="*********" className="input input-password" />
+					<button className="primary-button login-button" onClick={handleSubmit}> Log In </button>
+					<a href="/">Forgot my password</a>
+				</form>
+				<button className="secondary-button signup-button">Sign up</button>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
